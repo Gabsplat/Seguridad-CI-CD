d53dddb0fbb56500bc45015dafea3b20b74e0f16
Gabriel Pérez dIEZ <grperezdiez@gmail.com>
2024-10-02 11:46:30 -0300

    fixed jest

--- tests/jobService.test.js
+++ tests/jobService.test.js
@@ -2,24 +2,42 @@ require("dotenv").config();
 
 const { getJobs, getJobCategories } = require("../services/jobService");
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+);
+
 describe("Get Jobs", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
   test("Shouldn't get jobs if country is not provided", async () => {
-    return await expect(() => getJobs()).rejects.toThrow(Error);
+    await expect(() => getJobs()).rejects.toThrow(Error);
   });
 
   test("Shouldn't get jobs if country code is not supported", async () => {
     const country = "ss"; // not supported country code (SOUTH SUDAN)
-    return await expect(() => getJobs(country)).rejects.toThrow(Error);
+    await expect(() => getJobs(country)).rejects.toThrow(Error);
   });
 
   test("Should get jobs if everything is provided and correct", async () => {
     const country = "es"; // supported country code (SPAIN)
-    const jobs = await getJobs(country);
-    await expect(jobs).toHaveProperty("results");
+
+    const spyFetch = jest.spyOn(global, "fetch");
+
+    await getJobs(country);
+
+    expect(spyFetch).toHaveBeenCalled();
   });
 });
 
 describe("Get Job Categories", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
   test("Shouldn't get job categories if country is not provided", async () => {
     await expect(getJobCategories()).rejects.toThrow(Error);
   });
@@ -31,7 +49,11 @@ describe("Get Job Categories", () => {
 
   test("Should get jobs categories if everything is provided and correct", async () => {
     const country = "es"; // supported country code (SPAIN)
-    const jobCategories = await getJobCategories(country);
-    await expect(jobCategories).toHaveProperty("results");
+
+    const spyFetch = jest.spyOn(global, "fetch");
+
+    await getJobCategories(country);
+
+    expect(spyFetch).toHaveBeenCalled();
   });
 });
